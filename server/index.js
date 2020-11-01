f07063b6b6e55371f84a21d4580ed352519ec56e
Monil Bansal <bansalmonil7@gmail.com>
2020-11-01 21:44:53 +0530

    join page

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express           = require('express');
+const socketio          = require('socket.io');
+const http              = require('http');
+
+const router = require('./router');
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+io.on('connection' , (socket)=>{
+    console.log('We have a new socket connection!!!');
+
+    socket.on('join',()=>{
+        
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log('User has left!!!');
+    })
+});
+
+app.use(router);
+
+
+server.listen(PORT,() => {console.log(`Server has started on port : ${PORT}`)});
