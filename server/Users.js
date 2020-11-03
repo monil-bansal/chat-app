8b8a38ae10c04a773cf6c0e0181eeab10a382358
Monil Bansal <bansalmonil7@gmail.com>
2020-11-03 10:32:15 +0530

    logic

---
+++ server/Users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = ({id, name, room})=>{
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    const existingUser = users.find((user)=>  user.room ===room  && user.name === name);
+    if(existingUser){
+        return {error: 'Username is taken'};
+    }
+    const user = {id,name,room};
+    users.push(user);
+    return {user};
+}
+
+const removeUser = (id) => {
+    const idx  = users.findIndex((user)=> user.id===id);
+    if(idx!==-1){
+        return users.splice(idx,1)[0];
+    }
+}
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const express           = require('express');
-const socketio          = require('socket.io');
-const http              = require('http');
-
-const router = require('./router');
+const express                                            = require('express');
+const socketio                                           = require('socket.io');
+const http                                               = require('http');
+const {addUser, removeUser, getUser, getUsersInRoom}     = require('./Users')
+const router                                             = require('./router');
+const { use } = require('./router');
 
 
 const PORT = process.env.PORT || 5000;
@@ -13,10 +14,25 @@ const io = socketio(server);
 
 
 io.on('connection' , (socket)=>{
-    console.log('We have a new socket connection!!!');
+    socket.on('join',({name,room} ,callback  )=>{
+        const {error, user} = addUser({id:socket.id,name,room});
 
-    socket.on('join',()=>{
+        if(error) return callback(error);
         
+        console.log(`${user.name} has joined the room ${user.room}`)
+        socket.emit('message',{user: 'admin', text : `${user.name} welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message',{user:'admin', text :`${user.name} has joined!`});
+
+        socket.join(user.room);
+        callback();
+    })
+
+    socket.on('sendMessage',(message,callback)=>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message',{user:user.name, text : message});
+
+        callback();
     })
 
     socket.on('disconnect', ()=>{
