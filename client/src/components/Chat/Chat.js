4467f3b5ff519802245e25c43a188b2008823b93
Monil Bansal <bansalmonil7@gmail.com>
2020-11-04 23:31:01 +0530

    chat frontend

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react' ;
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css'
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
@@ -40,12 +44,14 @@ const Chat = ({location}) => {
             socket.emit('sendMessage', message , ()=>setMessage(''));
         }
     }
-    console.log(message);
-    console.log(messages);
+    console.log(messages)
     return (
-        <div className = "outterContainer">   
+        <div className = "outerContainer">   
             <div className = "container">
-                <input value={message} onChange= {(event) => setMessage(event.target.value)} onKeyPress={(event)=>event.key==='Enter'? sendMessage(event) :null} /> 
+                <InfoBar room = {room} />
+                <Messages messages = {messages} name=  {name}/>
+                <Input message = {message} setMessage = {setMessage} sendMessage = {sendMessage} />
+               
             </div>
         </div>
     )
---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Messages.css';
+import Message from './Message/Message';
+import ScrollToBottom from 'react-scroll-to-bottom';
+const Messages = ({messages,name}) => {
+    return (
+        <ScrollToBottom>
+            {messages.map((message,i) => <div key={i}><Message message = {message} name={name}/></div>)}
+        </ScrollToBottom>
+    )
+}
+
+
+export default Messages;
